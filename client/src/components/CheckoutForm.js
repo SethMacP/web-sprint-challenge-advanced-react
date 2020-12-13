514dc930be46483cdcf496ed1e529c4ca6001c31
SethMacP <macpherson.seth@gmail.com>
2020-12-13 09:26:18 -0800

    Custom hooks implimented, tests written, api call made and state set. Mvp seems complete?

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import {useForm} from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -14,17 +15,18 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [showSuccessMessage , values , handleChanges , handleSubmit] = useForm(initialValue);
+  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  // const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setShowSuccessMessage(true);
+  // };
 
   return (
     <>
@@ -34,6 +36,7 @@ const CheckoutForm = (props) => {
           First Name:
           <input
             name="firstName"
+            data-testid= "firstName"
             value={values.firstName}
             onChange={handleChanges}
           />
@@ -42,6 +45,7 @@ const CheckoutForm = (props) => {
           Last Name:
           <input
             name="lastName"
+            data-testid= "lastName"
             value={values.lastName}
             onChange={handleChanges}
           />
@@ -50,21 +54,33 @@ const CheckoutForm = (props) => {
           Address:
           <input
             name="address"
+            data-testid= "address"
             value={values.address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input 
+          name="city" 
+          data-testid= "city"
+          value={values.city} 
+          onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input 
+          name="state" 
+          data-testid= "state"
+          value={values.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input 
+          name="zip" 
+          data-testid= "zip"
+          value={values.zip} 
+          onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
